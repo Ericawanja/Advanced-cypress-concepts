b9276f3f32f2f443269f3b8eabde37d2373cf7f0
Ericawanja <wanjaerica@gmail.com>
2023-05-02 10:57:30 +0300

    copying data to clipboard

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then((win) => {
       cy.stub(win.navigator.geolocation, 'getCurrentPosition').as("getUserPosition").callsFake((cb) => {
         setTimeout(() => {
@@ -14,11 +14,23 @@ describe('share location', () => {
         }, 100)
 
       })
+      cy.stub(win.navigator.clipboard, 'writeText').as('copyToClipboard').resolves()
     });
-
+  })
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should('have.been.called')
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled')
     cy.get('[data-cy="actions"]').should('contain', 'Location fetched') //alternative for contains()
   });
+
+  it('shares the user location', () => {
+    cy.get('[data-cy="name-input"]').type('John Doe');
+    cy.get('[data-cy="get-loc-btn"]').click()
+    cy.get('[data-cy="share-loc-btn"]').click()
+    cy.get("@copyToClipboard").should("have.been.called")
+
+    //evaluatin stub arguments
+    cy.get('@copyToClipboard').should('have.been.calledWithMatch', new RegExp(`${37}.*${39}.*${encodeURI('John Doe')}`))
+  })
 });
