1f07aff00e72e5a33aedf3e6398fd9f39350f9aa
Ericawanja <wanjaerica@gmail.com>
2023-05-02 14:47:13 +0300

    spy and clock concepts

--- cypress/e2e/location.cy.js
+++ cypress/e2e/location.cy.js
@@ -1,20 +1,29 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
+
   beforeEach(() => {
+//getting the data from the fixtures
+cy.clock()
+
+cy.fixture("user-location.json").as("userLocation");
+
+
     cy.visit('/').then((win) => {
-      cy.stub(win.navigator.geolocation, 'getCurrentPosition').as("getUserPosition").callsFake((cb) => {
-        setTimeout(() => {
-          cb({
-            coords: {
-              latitude: 37,
-              longitude: 39
-            }
-          })
-        }, 100)
+
+      cy.get('@userLocation').then((fakePos)=>{
+        cy.stub(win.navigator.geolocation, 'getCurrentPosition').as("getUserPosition").callsFake((cb) => {
+          setTimeout(() => {
+            cb(fakePos)
+          }, 100)
+  
+        })
 
       })
+     
       cy.stub(win.navigator.clipboard, 'writeText').as('copyToClipboard').resolves()
+      cy.spy(win.localStorage, 'setItem').as('storeLocation');
+      cy.spy(win.localStorage, 'getItem').as('getStoredLocation')
     });
   })
   it('should fetch the user location', () => {
@@ -31,6 +40,24 @@ describe('share location', () => {
     cy.get("@copyToClipboard").should("have.been.called")
 
     //evaluatin stub arguments
-    cy.get('@copyToClipboard').should('have.been.calledWithMatch', new RegExp(`${37}.*${39}.*${encodeURI('John Doe')}`))
+    //cy.get('@copyToClipboard').should('have.been.calledWithMatch', new RegExp(`${37}.*${39}.*${encodeURI('John Doe')}`))
+
+    cy.get('@userLocation').then((fakePos)=>{
+      const {latitude, longitude} = fakePos.coords;
+      cy.get('@copyToClipboard').should('have.been.calledWithMatch', new RegExp(`${latitude}.*${longitude}.*${encodeURI('John Doe')}`))
+    })
+
+    cy.get('@storeLocation').should('have.been.called')
+    cy.get('[data-cy="share-loc-btn"]').click()
+    cy.get('@getStoredLocation').should('have.been.called')
+
+    //checking the dialog box
+
+    cy.get('[data-cy="info-message"]').should('be.visible')
+ 
+    cy.tick(2000);
+    cy.get('[data-cy="info-message"]').should("not.be.visible")
   })
+
+  // 
 });
